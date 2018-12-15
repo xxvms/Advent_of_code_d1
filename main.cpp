b9061902d95135da6ed9b4f51430252927c78b40
xxvms <tom@pcservicegroup.co.uk>
2018-12-15 22:52:43 +0000

    program resolves the puzzle from day 2 (not sure if there is more to resolve for this day)

--- main.cpp
+++ main.cpp
@@ -9,24 +9,81 @@
  * 3. remember to add const
 */
 
-void read_file(std::vector<int> &import_data);
-void summary(const std::vector<int> &data_to_sum, std::vector<int> &bank);
-//void twice(const std::vector<int> &data_to_find);
+void read_file_int(std::vector<int> &import_data);
+void read_file_str(std::vector<std::string> &import_data_str);
+void finding_summary(const std::vector<int> &data_to_sum, std::vector<int> &bank);
+void finding_letters(const std::vector<std::string> &import_dataSTR);
 
 
 int main() {
 
-    std::vector<int>data_file{}; // to keep all frequences from file
-    std::vector<int>bank{};      // to keep summary of each iteration
+    //std::vector<int>data_file{}; // to keep all frequences from file
+    //std::vector<int>bank{};      // to keep summary of each iteration
+    //read_file(data_file);
+    //finding_summary(data_file, bank);
 
-    read_file(data_file);
-    summary(data_file, bank);
+    // reading string from the file
+    std::vector<std::string> letters{};
+    read_file_str(letters);
+    finding_letters(letters);
+
+
 
     return 0;
 }
 
-// getting numbers from file
-void read_file(std::vector<int> &import_data){
+//getting letters from file STRING
+void read_file_str(std::vector<std::string> &import_data_str){
+    std::ifstream file("day2.txt");
+    std::string data;
+    while (std::getline(file, data)){
+        import_data_str.push_back(data);
+    }
+
+}
+
+void finding_letters(const std::vector<std::string> &import_dataSTR){
+
+    int count2 = 0;
+    int count3 = 0;
+    bool three = true;
+    bool two = true;
+
+
+    for (auto a: import_dataSTR){
+
+        std::sort(a.begin(), a.end());
+
+        for (auto item: a){
+
+            auto result = std::count(a.begin(), a.end(), item);
+
+            if (result > 1 && result < 4)
+            {
+                if (result == 3){
+                    if (three){
+                        count3++;
+                        three = false;
+                    }
+                } else if (result == 2){
+                    if (two){
+                        count2++;
+                        two = false;
+                    }
+                }
+            }
+        }
+        two = true;
+        three = true;
+    }
+
+    std::cout << "Pairs of 2: " << count2 << " & Pairs of 3: " << count3 << '\n';
+    std::cout << "Wynik " << count2 * count3;
+}
+
+
+// getting numbers from file INT
+void read_file_int(std::vector<int> &import_data){
 
     std::ifstream file("data.txt");
     std::string data;
@@ -36,7 +93,7 @@ void read_file(std::vector<int> &import_data){
     }
 }
 
-void summary(const std::vector<int> &data_to_sum, std::vector<int> &bank){
+void finding_summary(const std::vector<int> &data_to_sum, std::vector<int> &bank){
 
     int tmp = 0;
     bank.push_back(tmp);
